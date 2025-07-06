0430b8d475360325a00a85fbec378afb71d93760
zakoooook <zakomoise@gmail.com>
2025-07-06 20:14:49 +0000

    Update settings.py

--- pharmacie/pharmacie/settings.py
+++ pharmacie/pharmacie/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "pharmacie.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    "default": {
+   "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "railway",
         "USER": "postgres",
