0e38fcd0fd5b42e433118df95b42f4d2385787cc
zakoooook <zakomoise@gmail.com>
2025-07-06 21:16:57 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-# Création automatique du superuser si inexistant
+# Supprimer l'ancien superuser admin s'il existe, puis créer un nouveau
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='admin').exists(): \
-    User.objects.create_superuser('Admin', 'admin@example.com', 'Admin2025')" | python manage.py shell
+User.objects.filter(username='admin').delete(); \
+User.objects.create_superuser('admin', 'admin@example.com', 'Admin2025')" | python manage.py shell
 
 gunicorn pharmacie.wsgi:application --bind 0.0.0.0:$PORT
