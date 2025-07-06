a322591ab2a5109e4db0ce1a579636351a548acc
zakoooook <zakomoise@gmail.com>
2025-07-06 15:13:14 +0000

    Nettoyage des utilisateurs + reset local

--- pharmacie/settings.py
+++ pharmacie/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+
 # Application definition
 
 import os
@@ -133,3 +134,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+ALLOWED_HOSTS = ['pharmacie-app-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://pharmacie-app-production.up.railway.app']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
