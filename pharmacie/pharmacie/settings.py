21264206a0f2d9bc7462eb485e36b59c1dfee24a
zakoooook <zakomoise@gmail.com>
2025-07-06 14:37:22 +0000

    Update settings.py

--- pharmacie/pharmacie/settings.py
+++ pharmacie/pharmacie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5w&957#qlp(&#wch8igjtr=sth3^suzvja*9iosabqfglc1u=8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
