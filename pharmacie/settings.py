b64569d05fafb0069fafb569deb5fe7d858f8659
zakoooook <zakomoise@gmail.com>
2025-07-06 19:52:12 +0000

    Update settings.py

--- pharmacie/settings.py
+++ pharmacie/settings.py
@@ -85,12 +85,16 @@ WSGI_APPLICATION = "pharmacie.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "<railway>",
+        "USER": "<postgres>",
+        "PASSWORD": "<wUtBaDDMPcoqQVKqKtrnQBeXtedQAArI>",
+        "HOST": "<postgres.railway.internal>",
+        "PORT": "<5432>",
     }
+}
 }
 
 
